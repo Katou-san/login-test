5f596b66dd65f0c0f0abb668a2b87d12858395bc
katou <96455212+Katou-san@users.noreply.github.com>
2024-02-25 20:19:21 +0700

    Update server.js

--- src/Server/server.js
+++ src/Server/server.js
@@ -6,8 +6,7 @@ const { default: Axios } = require("axios");
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
-const fetch = (...value) =>
-  import("node-fetch").then(({ default: fetch }) => fetch(...value));
+
 const CLIENT_ID = "330b6c67acca4d537d39";
 const CLIENT_SECRET = "271b4db388bd198262ca1a501822d46083f9072a";
 
